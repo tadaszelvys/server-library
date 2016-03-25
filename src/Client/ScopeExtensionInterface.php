f902304f686fbb77aac6a07f106433972472cbae
Spomky <Spomky@users.noreply.github.com>
2016-03-25 11:42:04 +0100

    Update ScopeExtensionInterface.php

--- src/Client/ScopeExtensionInterface.php
+++ src/Client/ScopeExtensionInterface.php
@@ -21,33 +21,18 @@ interface ScopeExtensionInterface
     /**
      * Get available scopes.
      *
-     * @param \Psr\Http\Message\ServerRequestInterface $request The request
-     *
      * @return string[] The available scopes depending on the client and the server.
      *
      * @see https://tools.ietf.org/html/rfc6749#section-3.3
      */
-    public function getAvailableScopes(ServerRequestInterface $request = null);
-
-    /**
-     * Get the default scopes.
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request The request
-     *
-     * @return string[] The default scopes depending on the client and the server.
-     *
-     * @see https://tools.ietf.org/html/rfc6749#section-3.3
-     */
-    public function getDefaultScopes(ServerRequestInterface $request = null);
+    public function getAvailableScopes();
 
     /**
      * Get the scope policy.
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request The request
      *
      * @return string The scope policy depending on the client and the server.
      *
      * @see https://tools.ietf.org/html/rfc6749#section-3.3
      */
-    public function getScopePolicy(ServerRequestInterface $request = null);
+    public function getScopePolicy();
 }
